63a48607be9439420d68bcee3f781e3725982019
hobbyquaker <hobbyquaker@gmail.com>
2014-07-29 22:48:05 +0200

    gruntfile require tasks minor changes to jscs

--- Gruntfile.js
+++ Gruntfile.js
@@ -187,63 +187,9 @@ module.exports = function (grunt) {
 
         },
         // Javascript code styler
-        jscs: {
-            all: {
-                src: [
-                    srcDir + "*.js",
-                    srcDir + "lib/*.js",
-                    srcDir + "adapter/**/*.js",
-                    '!' + srcDir + 'node_modules/**/*.js',
-                    '!' + srcDir + 'adapter/admin/www/lib/**/*.js',
-                    '!' + srcDir + 'adapter/*/node_modules/**/*.js'
-                ],
-                options: {
-                    force: true,
-                    "requireCurlyBraces": ["else", "for", "while", "do", "try", "catch", "case", "default"], /*"if",*/
-                    "requireSpaceAfterKeywords": ["if", "else", "for", "while", "do", "switch", "return", "try", "catch"],
-//                    "requireSpaceBeforeBlockStatements": true,
-                    "requireParenthesesAroundIIFE": true,
-                    "requireSpacesInFunctionExpression": {"beforeOpeningCurlyBrace": true},
-                    "requireSpacesInAnonymousFunctionExpression": {"beforeOpeningRoundBrace": true, "beforeOpeningCurlyBrace": true},
-                    "requireSpacesInNamedFunctionExpression": {"beforeOpeningCurlyBrace": true},
-                    "requireSpacesInFunctionDeclaration": {"beforeOpeningCurlyBrace": true},
-                    "disallowMultipleVarDecl": true,
-                    "requireBlocksOnNewline": true,
-                    "disallowEmptyBlocks": true,
-                    "disallowSpacesInsideObjectBrackets": true,
-                    "disallowSpacesInsideArrayBrackets": true,
-                    "disallowSpaceAfterObjectKeys": true,
-                    "requireCommaBeforeLineBreak": true,
-                    //"requireAlignedObjectValues": "all",
-                    "requireOperatorBeforeLineBreak": ["?", "+", "-", "/", "*", "=", "==", "===", "!=", "!==", ">", ">=", "<", "<="],
-//                    "disallowLeftStickedOperators": ["?", "+", "-", "/", "*", "=", "==", "===", "!=", "!==", ">", ">=", "<", "<="],
-//                    "requireRightStickedOperators": ["!"],
-//                    "requireSpaceAfterBinaryOperators": ["?", "+", "/", "*", ":", "=", "==", "===", "!=", "!==", ">", ">=", "<", "<="],
-                    //"disallowSpaceAfterBinaryOperators": [","],
-                    "disallowSpaceAfterPrefixUnaryOperators": ["++", "--", "+", "-", "~", "!"],
-                    "disallowSpaceBeforePostfixUnaryOperators": ["++", "--"],
-                    "requireSpaceBeforeBinaryOperators": ["+", "-", "/", "*", "=", "==", "===", "!=", "!=="],
-                    "requireSpaceAfterBinaryOperators": ["?", ">", ",", ">=", "<=", "<", "+", "-", "/", "*", "=", "==", "===", "!=", "!=="],
-                    //"validateIndentation": 4,
-                    //"validateQuoteMarks": { "mark": "\"", "escape": true },
-                    "disallowMixedSpacesAndTabs": true,
-                    "disallowKeywordsOnNewLine": ["else", "catch"]
-                }
-            }
-        },
+        jscs: require('./tasks/jscs.js'),
         // Lint
-        jshint: {
-            options: {
-                force: true
-            },
-            all: [srcDir + "*.js",
-                  srcDir + "lib/*.js",
-                  srcDir + "adapter/**/*.js",
-                  '!' + srcDir + 'node_modules/**/*.js',
-                  '!' + srcDir + 'adapter/admin/www/lib/**/*.js',
-                  '!' + srcDir + 'adapter/*/node_modules/**/*.js'
-            ]
-        },
+        jshint: require('./tasks/jshint.js'),
         compress: {
             main: {
                 options: {
