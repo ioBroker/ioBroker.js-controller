c4ba88e59e75724f4ee36e6f0bbf5db1e88f4a49
GermanBluefox <dogafox@gmail.com>
2014-11-03 00:17:25 +0100

    (bluefox) add ping to repository

--- controller.js
+++ controller.js
@@ -7,7 +7,7 @@
  */
 
 // Change version in io-package.json and start grunt task to modify the version
-var version = '0.0.27';
+var version = '0.0.31';
 var title = 'io.js-controller';
 process.title = title;
 
