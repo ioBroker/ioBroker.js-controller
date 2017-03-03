716f1f2f9799e4fb3312252e80a2b3256441759f
GermanBluefox <dogafox@gmail.com>
2017-03-03 23:12:02 +0100

    (bluefox) tests

--- test/lib/testStates.js
+++ test/lib/testStates.js
@@ -66,9 +66,77 @@ function register(it, expect, context) {
     });
 
     // getState
-    // getStates
-    // delState
+    it(testName + 'Get local state', function (done) {
+        this.timeout(1000);
+        context.adapter.getState(gid, function (err) {
+            expect(err).to.be.not.ok;
 
+            context.adapter.getState(context.adapterShortName + '.0.' + gid, function (err, state) {
+                expect(err).to.be.null;
+                expect(state).to.be.ok;
+                expect(state.val).to.equal(3);
+                expect(state.ack).to.equal(true);
+
+                // ask for non-existing state
+                context.adapter.getState(gid + '5', function (err, state) {
+                    expect(err).to.be.not.ok;
+                    expect(state).to.be.not.ok;
+                    done();
+                });
+            });
+        });
+    });
+
+    // getStates
+    it(testName + 'Get local states', function (done) {
+        this.timeout(1000);
+        context.adapter.getStates('*', function (err, states) {
+            expect(err).to.be.not.ok;
+            expect(states).to.be.an('object');
+            expect(states[context.adapterShortName + '.0.' + gid]).to.be.ok;
+            expect(states[context.adapterShortName + '.0.' + gid].val).to.equal(3);
+            expect(states[context.adapterShortName + '.0.' + gid].ack).equal(true);
+
+            context.adapter.getStates('abc*', function (err, states) {
+                expect(err).to.be.not.ok;
+                expect(states).to.be.an('object');
+                var found = false;
+                for(var a in states) {
+                    found = true;
+                }
+                expect(found).to.be.false;
+
+                var iid = gid;
+                context.adapter.getStates(gid.substring(0, gid.length - 2) + '*', function (err, states) {
+                    expect(err).to.be.not.ok;
+                    expect(states).to.be.an('object');
+                    expect(states[context.adapterShortName + '.0.' + gid]).to.be.ok;
+                    expect(states[context.adapterShortName + '.0.' + gid].val).to.equal(3);
+                    expect(states[context.adapterShortName + '.0.' + gid].ack).equal(true);
+
+                    done();
+                });
+            });
+        });
+    });
+    // delState
+    it(testName + 'Delete local state', function (done) {
+        this.timeout(1000);
+        context.adapter.delState(gid, function (err) {
+            expect(err).to.be.not.ok;
+
+            context.adapter.getState(gid, function (err, state) {
+                expect(err).to.be.not.ok;
+                expect(state).to.be.not.ok;
+
+                context.adapter.delState(gid, function (err) {
+                    expect(err).to.be.not.ok;
+
+                    done();
+                });
+            });
+        });
+    });
     // setStateChanged
 
     // setForeignState
