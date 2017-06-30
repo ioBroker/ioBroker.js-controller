ed783c1a495e4287797e65fb8a4508755225dde9
Ingo Fischer <github@fischer-ka.de>
2017-06-30 16:20:40 +0200

    test further with sock

--- test/redis-socket/setup-socket.sh
+++ test/redis-socket/setup-socket.sh
@@ -2,5 +2,5 @@
 redis-cli SHUTDOWN
 sudo redis-server ${TRAVIS_BUILD_DIR}/test/redis-socket/redis-socket.config &
 sleep 15
-redis-cli PING
+redis-cli -s /tmp/redis.sock PING
 npm run test-redis-socket
