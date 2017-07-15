5ecb8f649188666e681eab5f9b746d5998399062
GermanBluefox <dogafox@gmail.com>
2017-07-15 23:06:14 +0200

    (bluefox) catch semver error

--- controller.js
+++ controller.js
@@ -682,7 +682,12 @@ function collectDiagInfo(type, callback) {
                             if (!semver) semver = require('semver');
 
                             doc.rows.sort(function (a, b) {
-                                return semver.lt(a.value.common.installedVersion, b.value.common.installedVersion);
+                                try {
+                                    return semver.lt(a.value.common.installedVersion, b.value.common.installedVersion);
+                                } catch (e) {
+                                    logger.error('host.' + hostname + ' Invalid versions: ' + a.value.common.installedVersion + '[' + a.value.common.name + '] or ' + b.value.common.installedVersion + '[' + b.value.common.name + ']');
+                                    return 0;
+                                }
                             });
 
                             // Read installed versions of all hosts
@@ -691,7 +696,6 @@ function collectDiagInfo(type, callback) {
                                     version:  doc.rows[i].value.common.installedVersion,
                                     platform: doc.rows[i].value.common.platform,
                                     type:     doc.rows[i].value.native.os.platform
-
                                 });
                             }
                         }
