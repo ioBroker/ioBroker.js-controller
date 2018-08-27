7951af55d0dba04ee951f7868428b33796ee3073
GermanBluefox <dogafox@gmail.com>
2018-08-27 23:12:20 +0200

    (bluefox) grunt => gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,243 @@
 'use strict';
 
-var gulp      = require('gulp');
-var fs        = require('fs');
-var pkg       = require('./package.json');
-var iopackage = require('./io-package.json');
-var version   = (pkg && pkg.version) ? pkg.version : iopackage.common.version;
+const gulp      = require('gulp');
+const fs        = require('fs');
+const pkg       = require('./package.json');
+const iopackage = require('./io-package.json');
+const version   = (pkg && pkg.version) ? pkg.version : iopackage.common.version;
+const request   = require('request');
+const replace   = require('gulp-replace');
+const jsdoc     = require('gulp-jsdoc3');
 
+const appName   = getAppName();
+const srcDir    = __dirname + '/';
 
-gulp.task('updateRepo1', function () {
-    var sources = JSON.parse(fs.readFileSync(__dirname + '/conf/sources-dist.json'));
+function getAppName() {
+    const parts = __dirname.replace(/\\/g, '/').split('/');
+    return parts[parts.length - 1].split('.')[0].toLowerCase();
+}
 
-    for (var adapter in sources) {
-        if (!sources.hasOwnProperty(adapter)) continue;
-        if (sources[adapter].meta) {
-            if (sources[adapter].meta.substring(0, 'http://'.length) === 'http://') {
-                grunt.task.run(['http:get_http:' + sources[adapter].meta.substring('http://'.length) + ':' + adapter]);
-            } else
-            if (sources[adapter].meta.substring(0, 'https://'.length) === 'https://') {
-                grunt.task.run(['http:get_https:' + sources[adapter].meta.substring('https://'.length) + ':' + adapter]);
+function processAdapters(sources, adapters, callback, result) {
+    result = result || {};
+    if (!adapters || !adapters.length) {
+        callback(result);
+    } else {
+        const adapter = adapters.shift();
+        request(sources[adapter].meta, (err, status, body) => {
+            try {
+                result[adapter] = JSON.parse(body).common;
+            } catch (e) {
+                console.error('Cannot parse "' + adapter + '": ' + e);
             }
+            setImmediate(processAdapters, sources, adapters, callback, result);
+        });
+    }
+}
+
+gulp.task('updateRepo', done => {
+    const sources = JSON.parse(fs.readFileSync(__dirname + '/conf/sources-dist.json'));
+    const adapters = Object.keys(sources);
+
+    processAdapters(sources, adapters, result => {
+        for (const adapter in result) {
+            if (!result.hasOwnProperty(adapter)) continue;
+            const meta = sources[adapter].meta;
+            const url  = sources[adapter].url;
+            sources[adapter] = result[adapter];
+            sources[adapter].meta = meta;
+            sources[adapter].url  = url;
+            sources[adapter].icon = sources[adapter].extIcon;
+        }
+        fs.writeFileSync(__dirname + '/conf/sources-dist.json', JSON.stringify(sources, null, 2));
+        done();
+    });
+});
+
+gulp.task('renameFiles', done => {
+    fs.unlink(__dirname + '/lib/img/iobroker.png');
+    if (fs.existsSync(__dirname + '/iobroker'))                 fs.renameSync(__dirname + '/iobroker',                __dirname + '/' + appName);
+    if (fs.existsSync(__dirname + '/_service_iobroker.bat'))    fs.renameSync(__dirname + '/_service_iobroker.bat',   __dirname + '/_service_' + appName + '.bat');
+    if (fs.existsSync(__dirname + '/iobroker.bat'))             fs.renameSync(__dirname + '/iobroker.bat',            __dirname + '/' + appName + '.bat');
+    if (fs.existsSync(__dirname + '/iobroker.js'))              fs.renameSync(__dirname + '/iobroker.js',             __dirname + '/' + appName + '.js');
+    if (fs.existsSync(__dirname + '/conf/iobroker-dist.json'))  fs.renameSync(__dirname + '/conf/iobroker-dist.json', __dirname + '/conf/' + appName + '-dist.json');
+    done();
+});
+
+gulp.task('replaceName', done => {
+    const patterns = [
+        {
+            match:       /iobroker/gi,
+            replacement: appName
+        },
+        {
+            match:       /"iobroker\.admin": "\*"/i,
+            replacement: ''
+        }
+    ];
+    const files = [
+        {
+            expand:  true,
+            flatten: true,
+            src:     [
+                srcDir + '*.*',
+                srcDir + '.travis.yml',
+                srcDir + '.npmignore',
+                srcDir + '.gitignore'
+            ],
+            dest:    srcDir
+        },
+        {
+            expand:  true,
+            flatten: true,
+            src:     [
+                srcDir + 'admin/*.*',
+                '!' + srcDir + 'admin/*.png'
+            ],
+            dest:    srcDir + 'admin'
+        },
+        {
+            expand:  true,
+            flatten: true,
+            src:     [
+                srcDir + 'lib/*.*'
+            ],
+            dest:    srcDir + 'lib'
+        },
+        {
+            expand:  true,
+            flatten: true,
+            src:     [
+                srcDir + 'example/*.*'
+            ],
+            dest:    srcDir + 'example'
+        },
+        {
+            expand:  true,
+            flatten: true,
+            src:     [
+                srcDir + 'www/*.*'
+            ],
+            dest:    srcDir + 'www'
+        },
+        {
+            expand:  true,
+            flatten: true,
+            src:     [
+                srcDir + 'conf/*.*'
+            ],
+            dest:    srcDir + 'conf'
+        }
+    ];
+
+    const tasks = [];
+
+    files.forEach(task =>
+        tasks.push(gulp.src(task.src)
+            .pipe(replace(patterns[0].match, patterns[0].replacement))
+            .pipe(replace(patterns[1].match, patterns[1].replacement))
+            .pipe(gulp.dest(task.dest))
+    ));
+
+    return Promise.all(tasks);
+});
+
+gulp.task('replaceCore', done => {
+    const patterns = [
+        {
+            match: /var version = '[.0-9]*';/g,
+            replacement: "var version = '" + iopackage.common.version + "';"
+        },
+        {
+            match: /"version": "[.0-9]*",/g,
+            replacement: '"version": "' + iopackage.common.version + '",'
+        }
+    ];
+    const files = [
+        srcDir + 'controller.js',
+        srcDir + 'package.json'
+    ];
+    for (let f = 0; f < files.length; f++) {
+        if (fs.existsSync(files[f])) {
+            let text = fs.readFileSync(files[f]).toString('utf8');
+            patterns.forEach(pattern => {
+                text = text.replace(pattern.match, pattern.replacement);
+            });
+            fs.writeFileSync(files[f], text);
+
         }
     }
+    done();
 });
+gulp.task('cleanRepo', done => {
+    const sources = JSON.parse(fs.readFileSync(__dirname + '/conf/sources-dist.json'));
+    for (const adapter in sources) {
+        if (!sources.hasOwnProperty(adapter)) continue;
+        const meta = sources[adapter].meta;
+        const url  = sources[adapter].url;
+        const icon = sources[adapter].icon;
+
+        sources[adapter] = {};
+        if (meta) sources[adapter].meta = meta;
+        if (url)  sources[adapter].url  = url;
+        if (icon) sources[adapter].icon = icon;
+    }
+    fs.writeFileSync(__dirname + '/conf/sources-dist.json', JSON.stringify(sources, null, 2));
+    done();
+});
+
+gulp.task('updateReadme', done => {
+    const readme = fs.readFileSync('CHANGELOG.md');
+    if (readme.indexOf(version) === -1) {
+        const timestamp = new Date();
+        const date = timestamp.getFullYear() + '-' +
+            ('0' + (timestamp.getMonth() + 1).toString(10)).slice(-2) + '-' +
+            ('0' + (timestamp.getDate()).toString(10)).slice(-2);
+
+        let news = '';
+        if (iopackage.common.whatsNew) {
+            for (let i = 0; i < iopackage.common.whatsNew.length; i++) {
+                if (typeof iopackage.common.whatsNew[i] === 'string') {
+                    news += '* ' + iopackage.common.whatsNew[i] + '\n';
+                } else {
+                    news += '* ' + iopackage.common.whatsNew[i].en + '\n';
+                }
+            }
+        }
+        fs.writeFileSync('CHANGELOG.md', '# ' + version + ' (' + date + ')\n' + news + '\n' + readme);
+    }
+    done();
+});
+
+gulp.task('jsdoc', done => {
+    gulp.src(['lib/adapter.js'], {read: false})
+        .pipe(jsdoc(
+            {
+                tags: {
+                    allowUnknownTags: true
+                },
+                opts: {
+                    destination: "./doc"
+                },
+                plugins: [
+                    'plugins/markdown'
+                ],
+                templates: {
+                    cleverLinks: false,
+                    monospaceLinks: false,
+                    default: {
+                        outputSourceFiles: true
+                    },
+                    path: 'ink-docstrap',
+                    theme: 'cerulean',
+                    navType: 'vertical',
+                    linenums: true,
+                    //dateFormat: "MMMM Do YYYY, h:mm:ss a"
+                }
+            },
+            done));
+});
+
+gulp.task('default',    ['replaceCore', 'updateReadme']);
+gulp.task('p',          ['replaceCore', 'updateReadme']);
+gulp.task('rename',     ['replace:name', 'renameFiles']);
+gulp.task('doc',        ['jsdoc']);
