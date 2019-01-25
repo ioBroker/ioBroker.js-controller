ioBroker/ioBroker.js-controller

commit 23f02c1df5aad06649643b68a205a8dca3138e3d

(bluefox) read hash of sources.json before read the whole file

lib/setup/setupRepo.js
@@ -35,42 +35,45 @@ function Repo(options) {
     const objects = options.objects;
     const states  = options.states;
 
-    function updateRepo(repoUrl, callback) {
+    function download(downloads, sources, result, sourcesHash, callback) {
+        if (downloads.length < 1) {
+            console.log('update done');
+            callback && callback(result, sourcesHash);
+        } else {
+            const name = downloads.pop();
+
+            if (sources[name].version) {
+                result[name] = sources[name];
+            } else if (sources[name].meta) {
+                return tools.getJson(sources[name].meta, ioPack => {
+                    if (ioPack && ioPack.common) {
+                        result[name] = extend(true, sources[name], ioPack.common);
+                    }
+                    setImmediate(downloads, sources, result, sourcesHash, callback);
+                });
+            } else if (sources[name].url) {
+                console.log('Cannot get version of "' + name + '".');
+                result[name] = sources[name];
+            } else {
+                console.log('Cannot get any information of "' + name + '". Ignored.');
+            }
+            setImmediate(downloads, sources, result, sourcesHash, callback);
+        }
+    }
+
+    function updateRepo(repoUrl, sourcesHash, callback) {
         const result = {};
+        if (typeof sourcesHash === 'function') {
+            callback = sourcesHash;
+            sourcesHash = '';
+        }
 
         if (!repoUrl || typeof repoUrl !== 'object') {
-            tools.getRepositoryFile(repoUrl, (err, sources) => updateRepo(sources, callback));
+            tools.getRepositoryFile(repoUrl, (err, sources, sourcesHash) => updateRepo(sources, sourcesHash, callback));
             return;
         }
         const sources = repoUrl;
         const downloads = [];
-
-        function download() {
-            if (downloads.length < 1) {
-                console.log('update done');
-                if (callback) callback(result);
-            } else {
-                const name = downloads.pop();
-
-                if (sources[name].version) {
-                    result[name] = sources[name];
-                } else if (sources[name].meta) {
-                    tools.getJson(sources[name].meta, ioPack => {
-                        if (ioPack && ioPack.common) {
-                            result[name] = extend(true, sources[name], ioPack.common);
-                        }
-                        setImmediate(download);
-                    });
-                    return;
-                } else if (sources[name].url) {
-                    console.log('Cannot get version of "' + name + '".');
-                    result[name] = sources[name];
-                } else {
-                    console.log('Cannot get any information of "' + name + '". Ignored.');
-                }
-                setImmediate(download);
-            }
-        }
 
         // Read repository file, local or by url
         for (const name in sources) {
@@ -78,7 +81,7 @@ function Repo(options) {
             downloads.push(name);
         }
 
-        download(/*sources*/);
+        download(downloads, sources, result, sourcesHash, callback);
     }
 
     this.showRepo = function (repoUrl, flags, callback) {
@@ -151,12 +154,14 @@ function Repo(options) {
                             if (typeof obj.native.repositories[repoUrl] === 'string') {
                                 obj.native.repositories[repoUrl] = {
                                     link: obj.native.repositories[repoUrl],
-                                    json: null
+                                    json: null,
+                                    hash: ''
                                 };
                             }
 
-                            updateRepo(obj.native.repositories[repoUrl].link, sources => {
+                            updateRepo(obj.native.repositories[repoUrl].link, (sources, sourcesHash) => {
                                 obj.native.repositories[repoUrl].json = sources;
+                                obj.native.repositories[repoUrl].hash = sourcesHash;
                                 obj.from = 'system.host.' + tools.getHostName() + '.cli';
                                 obj.ts = new Date().getTime();
                                 objects.setObject(obj._id, obj, () => {

controller.js
@@ -1,1 +1,8 @@
+/**
+ *      Redirect for older installations
+ *
+ *      Copyright 2018-2019 bluefox <dogafox@gmail.com>,
+ *      MIT License
+ *
+ */
 require('./main');

gulpfile.js
@@ -1,3 +1,10 @@
+/**
+ *      Gulp tasks: update default repo files, create template for news.
+ *
+ *      Copyright 2018-2019 bluefox <dogafox@gmail.com>,
+ *      MIT License
+ *
+ */
 'use strict';
 
 const gulp      = require('gulp');
@@ -169,6 +176,7 @@ gulp.task('replaceCore', done => {
     }
     done();
 });
+
 gulp.task('cleanRepo', done => {
     const sources = JSON.parse(fs.readFileSync(__dirname + '/conf/sources-dist.json'));
     for (const adapter in sources) {
@@ -238,7 +246,7 @@ gulp.task('jsdoc', done => {
             done));
 });
 
-gulp.task('default',    ['replaceCore', 'updateReadme']);
-gulp.task('p',          ['replaceCore', 'updateReadme']);
-gulp.task('rename',     ['replace:name', 'renameFiles']);
-gulp.task('doc',        ['jsdoc']);
+gulp.task('default',    gulp.series('replaceCore', 'updateReadme'));
+gulp.task('p',          gulp.series('replaceCore', 'updateReadme'));
+gulp.task('rename',     gulp.series('replace:name', 'renameFiles'));
+gulp.task('doc',        gulp.series('jsdoc'));

lib/multihostClient.js
@@ -1,3 +1,12 @@
+/**
+ *      Multihost client
+ *
+ *      Slave multihost functionality
+ *
+ *      Copyright 2014-2019 bluefox <dogafox@gmail.com>,
+ *      MIT License
+ *
+ */
 'use strict';
 
 const dgram  = require('dgram');

lib/multihostServer.js
@@ -1,3 +1,13 @@
+/**
+ *      Multihost client
+ *
+ *      Master multihost functionality
+ *
+ *      Copyright 2014-2019 bluefox <dogafox@gmail.com>,
+ *      MIT License
+ *
+ */
+
 'use strict';
 const dgram          = require('dgram');
 const port           = 50005;

lib/setup.js
@@ -1427,7 +1427,7 @@ function processCommand(command, args, params, callback) {
             json.dependencies[tools.appName + '.js-controller'] = '*';
             json.dependencies[tools.appName + '.admin']         = '*';
 
-            tools.getRepositoryFile(null, (_err, sources) => {
+            tools.getRepositoryFile(null, null, (_err, sources, _sourcesHash) => {
                 if (sources) {
                     for (const s in sources) {
                         if (sources.hasOwnProperty(s)) {
@@ -2205,8 +2205,12 @@ function getRepository(repoUrl, params, callback) {
                             if (!repos.native.repositories[active].json) {
                                 console.log('Update repository "' + active + '" under "' + repos.native.repositories[active].link + '"');
                                 // Load it
-                                tools.getRepositoryFile(repos.native.repositories[active].link, (_err, sources) => {
+                                tools.getRepositoryFile(repos.native.repositories[active].link, {
+                                    hash: repos.native.repositories[active].hash, 
+                                    sources: repos.native.repositories[active].json
+                                }, (_err, sources, sourcesHash) => {
                                     repos.native.repositories[active].json = sources;
+                                    repos.native.repositories[active].hash = sourcesHash;
                                     repos.from = 'system.host.' + tools.getHostName() + '.cli';
                                     repos.ts = new Date().getTime();
                                     // Store uploaded repo

lib/tools.js
@@ -328,7 +328,7 @@ function createUuid(_objects, callback) {
     let _uuid;
     _objects.getObject('system.user.admin', (err, obj) => {
         if (err || !obj) {
-            password = password || require(__dirname + '/password');
+            password = password || require('./password');
 
             // Default Password for user 'admin' is application name in lower case
             password(getAppName()).hash(null, null, (err, res) => {
@@ -436,14 +436,14 @@ function getJson(urlOrPath, agent, callback) {
             urlOrPath.substring(0, 'https://'.length) === 'https://') {
             request({url: urlOrPath, timeout: 10000, gzip: true, headers: {'User-Agent': agent}}, (error, response, body) => {
                 if (error || !body || response.statusCode !== 200) {
-                    console.log('Cannot download json from ' + urlOrPath + '. Error: ' + (error || body));
+                    console.warn('Cannot download json from ' + urlOrPath + '. Error: ' + (error || body));
                     if (callback) callback(null, urlOrPath);
                     return;
                 }
                 try {
                     sources = JSON.parse(body);
                 } catch (e) {
-                    console.log('Json file is invalid on ' + urlOrPath);
+                    console.error('Json file is invalid on ' + urlOrPath);
                     if (callback) callback(null, urlOrPath);
                     return;
                 }
@@ -721,7 +721,54 @@ function _getRepositoryFile(sources, path, callback) {
     }
 }
 
-// Get list of all adapters and controller in some repository file or in /conf/source-dist.json
+function _checkRepositoryFileHash(urlOrPath, additionalInfo, callback) {
+    // read hash of file
+    if (urlOrPath.startsWith('http://') || urlOrPath.startsWith('https://')) {
+        urlOrPath = urlOrPath.replace(/\.json$/, '-hash.json');
+        let json = null;
+        request({url: urlOrPath, timeout: 10000, gzip: true}, (error, response, body) => {
+            if (error || !body || response.statusCode !== 200) {
+                console.warn('Cannot download json from ' + urlOrPath + '. Error: ' + (error || body));
+            } else {
+                try {
+                    json = JSON.parse(body);
+                } catch (e) {
+                    console.error('Json file is invalid on ' + urlOrPath);
+                }
+            }
+            // If new hash was read and old hash is known
+            if (json && json.hash && additionalInfo && additionalInfo.hash && additionalInfo.sources) {
+                // compare hash
+                if (json.hash === additionalInfo.hash) {
+                    // hash not changed, use actual sources
+                    callback(null, additionalInfo.sources, json.hash);
+                } else {
+                    // hash changed => force download of new sources
+                    callback(null, null, json.hash);
+                }
+            } else {
+                callback(null, additionalInfo.sources, (json && json.hash) || '');
+            }
+        }).on('error', _error => {
+            //console.log('Cannot download json from ' + urlOrPath + '. Error: ' + error);
+            //if (callback) callback(null, urlOrPath);
+        });
+    } else {
+        // it is a file and file has not hash
+        callback(null, null, 0);
+    }
+}
+
+/**
+ * Get list of all adapters and controller in some repository file or in /conf/source-dist.json
+ *
+ * @alias getRepositoryFile
+ * @memberof tools
+ * @param {string} urlOrPath URL stargin with http:// or https:// or local file link
+ * @param {object} additionalInfo destination object
+ * @param {function} callback function (err, sources, actualHash) { }
+ *
+ */
 function getRepositoryFile(urlOrPath, additionalInfo, callback) {
     let sources = {};
     let path =    '';
@@ -774,22 +821,30 @@ function getRepositoryFile(urlOrPath, additionalInfo, callback) {
             agent = `${additionalInfo.name}, RND: ${randomID}, Node:${additionalInfo.node}, V:${additionalInfo.controller}`;
         }
 
-        getJson(urlOrPath, agent, sources => {
-            if (sources) {
-                for (const s in sources) {
-                    if (sources.hasOwnProperty(s) && additionalInfo[s] && additionalInfo[s].published) {
-                        sources[s].published = additionalInfo[s].published;
-                    }
-                }
-                setImmediate(() =>
-                    _getRepositoryFile(sources, path, err => {
-                        if (err) console.error('[' + new Date() + '] ' + err);
-                        if (typeof callback === 'function') callback(err, sources);
-                    }));
+        // load hash of file first to not load the whole 1MB of sources
+        _checkRepositoryFileHash(urlOrPath, additionalInfo, (err, sources, actualSourcesHash) => {
+            if (!err && sources) {
+                // Source file was not changed
+                typeof callback === 'function' && callback(err, sources, actualSourcesHash);
             } else {
-                if (typeof callback === 'function') {
-                    callback('Cannot read "' + urlOrPath + '"', {});
-                }
+                getJson(urlOrPath, agent, sources => {
+                    if (sources) {
+                        for (const s in sources) {
+                            if (sources.hasOwnProperty(s) && additionalInfo[s] && additionalInfo[s].published) {
+                                sources[s].published = additionalInfo[s].published;
+                            }
+                        }
+                        setImmediate(() =>
+                            _getRepositoryFile(sources, path, err => {
+                                err && console.error('[' + new Date() + '] ' + err);
+                                typeof callback === 'function' && callback(err, sources, actualSourcesHash);
+                            }));
+                    } else {
+                        if (typeof callback === 'function') {
+                            callback('Cannot read "' + urlOrPath + '"', {});
+                        }
+                    }
+                });
             }
         });
     }

commit 0450ceec943a36bb236df4e497c671f8414bb19e

Fix error introduced by 23f02c1

lib/setup/setupRepo.js
@@ -49,7 +49,7 @@ function Repo(options) {
                     if (ioPack && ioPack.common) {
                         result[name] = extend(true, sources[name], ioPack.common);
                     }
-                    setImmediate(downloads, sources, result, sourcesHash, callback);
+                    setImmediate(download, downloads, sources, result, sourcesHash, callback);
                 });
             } else if (sources[name].url) {
                 console.log('Cannot get version of "' + name + '".');
@@ -57,7 +57,7 @@ function Repo(options) {
             } else {
                 console.log('Cannot get any information of "' + name + '". Ignored.');
             }
-            setImmediate(downloads, sources, result, sourcesHash, callback);
+            setImmediate(download, downloads, sources, result, sourcesHash, callback);
         }
     }
 