53cf1c4412f1301d3854afb55809c1173b220168
Ingo Fischer <github@fischer-ka.de>
2020-09-22 16:41:09 +0200

    * try fix tests

--- test/testAdapterStatesRedis.js
+++ test/testAdapterStatesRedis.js
@@ -16,38 +16,18 @@ const statesConfig = {
     host:           '127.0.0.1',
     port:           6379
 };
-let isExecute = require('fs').existsSync(__dirname  + '/../lib/objects/objectsInRedis.js');
-if (!isExecute) {
-    try {
-        const path = require.resolve('iobroker.objects-redis');
-        isExecute = !!path;
-    } catch (e) { /* OK */ }
-}
-let objectsConfig;
-if (isExecute) {
-    objectsConfig = {
-        dataDir:        __dirname + '/../tmp/data',
-        options : {
-            auth_pass: null,
-            retry_max_delay: 100
-        },
-        redisNamespace: 'test',
-        type:           'redis',
-        host:           '127.0.0.1',
-        port:           6379
-    };
-} else {
-    objectsConfig = {
-        dataDir:        dataDir,
-        type:           'file',
-        host:           '127.0.0.1',
-        port:           19002,
-        user:           '',
-        pass:           '',
-        noFileCache:    true,
-        connectTimeout: 2000
-    };
-}
+
+let objectsConfig = {
+    dataDir:        __dirname + '/../tmp/data',
+    options : {
+        auth_pass: null,
+        retry_max_delay: 100
+    },
+    redisNamespace: 'test',
+    type:           'redis',
+    host:           '127.0.0.1',
+    port:           6379
+};
 
 // states in REDIS, objects in files
 testAdapter({
