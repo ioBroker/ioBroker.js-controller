ff14ea4d0abcd03b89c35e90892891ba70f57b8d
Dominic Griesel <d.griesel@gmx.net>
2020-12-07 22:29:39 +0100

    allow more characters for legacy reasons

--- test/testTools.js
+++ test/testTools.js
@@ -6,7 +6,7 @@ const { FORBIDDEN_CHARS } = require('../lib/tools');
 describe('test tools.js helpers', () => {
     it('FORBIDDEN_CHARS', () => {
         const tests = [
-            { input: 'abc^def.0.1.foo-bar_', expected: 'abc_def.0.1.foo-bar_' },
+            { input: 'abc?def.0.1.foo-bar_', expected: 'abc_def.0.1.foo-bar_' },
             { input: 'ݑ', expected: '_' }, // Arabic Letter Beh with Dot Below and Three Dots Above (is an "other letter")
             { input: 'ⴃ', expected: 'ⴃ' }, // Georgian Small Letter Don (is a lowercase letter)
             { input: 'Ϣ', expected: 'Ϣ' }, // Coptic Capital Letter Shei (is a uppercase letter)
