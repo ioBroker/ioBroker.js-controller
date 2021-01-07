e92ad2b3f1d4bfbeef9b35cec2f427dc3129880e
GermanBluefox <dogafox@gmail.com>
2021-01-08 00:25:34 +0100

    Show instance ID by restart

--- main.js
+++ main.js
@@ -2502,7 +2502,7 @@ function restartInstances(instances, cb) {
         cb && cb();
     } else {
         const id = instances.shift();
-        logger.info(`${hostLogPrefix} instance restarted because the "let's encrypt" certificates were updated`);
+        logger.info(`${hostLogPrefix} instance "${id} restarted because the "let's encrypt" certificates were updated`);
         stopInstance(id, () => {
             startInstance(id);
             setTimeout(() => restartInstances(instances, cb), 3000);
