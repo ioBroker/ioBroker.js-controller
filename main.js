ab796180ee1425a7922235f6cea25803300bf4fa
Max Hauser <moritz.heusinger@gmail.com>
2021-04-16 17:23:03 +0200

    if left memory reaches threshold, also add a notification (#1283)
    * if left memory reaches threshold, also add a notification
    
    - fixes #1269
    
    * reduce some duplicate lines but eval second if statement always

--- main.js
+++ main.js
@@ -3153,6 +3153,12 @@ function startScheduledInstance(callback) {
     processNextScheduledInstance();
 }
 
+/**
+ * Start given instance
+ * @param {string} id - id of instance, like 'system.adapter.hm-rpc.0'
+ * @param {boolean} wakeUp
+ * @returns {Promise<void>}
+ */
 async function startInstance(id, wakeUp) {
     if (isStopping || !connected) {
         return;
@@ -3292,11 +3298,20 @@ async function startInstance(id, wakeUp) {
     }
 
     // default: if less than 100 MB log warning, less than 50 MB log error, but check config first
-    if (availableMemMB !== undefined && availableMemMB < (typeof config.system.memLimitError === 'number' ? config.system.memLimitError : 50)) {
-        logger.error(`${hostLogPrefix} Your system has only ${availableMemMB} MB RAM left available and an additional adapter process is started. Please check your system, settings and active instances to prevent swapping and Out-Of-Memory situations!`);
-        logger.error(`${hostLogPrefix} In future versions, the adapter might not be started!`);
-    } else if (availableMemMB !== undefined && availableMemMB < (typeof config.system.memLimitWarn === 'number' ? config.system.memLimitWarn : 100)) {
-        logger.warn(`${hostLogPrefix} Your system has only ${availableMemMB} MB RAM left available and an additional adapter process is started. Please check your system, settings and active instances to prevent swapping and Out-Of-Memory situations!`);
+    if (availableMemMB !== undefined && availableMemMB < (typeof config.system.memLimitWarn === 'number' ? config.system.memLimitWarn : 100)) {
+        if (availableMemMB < (typeof config.system.memLimitError === 'number' ? config.system.memLimitError : 50)) {
+            logger.error(`${hostLogPrefix} Your system has only ${availableMemMB} MB RAM left available and an additional adapter process is started. Please check your system, settings and active instances to prevent swapping and Out-Of-Memory situations!`);
+            logger.error(`${hostLogPrefix} In future versions, the adapter might not be started!`);
+        } else {
+            logger.warn(`${hostLogPrefix} Your system has only ${availableMemMB} MB RAM left available and an additional adapter process is started. Please check your system, settings and active instances to prevent swapping and Out-Of-Memory situations!`);
+        }
+
+        // add it to notifications for popup
+        try {
+            await notificationHandler.addMessage('system', 'memIssues', `Your system has only ${availableMemMB} MB RAM left available and an additional adapter process is started. Please check your system, settings and active instances to prevent swapping and Out-Of-Memory situations!`, `system.host.${hostname}`);
+        } catch (e) {
+            logger.warn(`${hostLogPrefix} Could not add OOM notification: ${e.message}`);
+        }
     }
 
     //noinspection JSUnresolvedVariable
