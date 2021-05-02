b27fbe4b2e9a1b77b13984ba1aacd83739418306
Bluefox <dogafox@gmail.com>
2021-05-02 16:44:59 +0200

    Update update.sh

--- engine/update.sh
+++ engine/update.sh
@@ -1,11 +1,11 @@
 cd /opt/doc-auto
+/usr/bin/git checkout .
 /usr/bin/git pull
+cp /opt/configBackup.js /opt/doc-auto/engine/config.js
 cp engine/update.sh ../updateSite.sh
 chmod 744 ../updateSite.sh
-cd engine/front-end/docu
-/usr/bin/git pull
-cd ../../..
 cd engine/
 /usr/bin/npm i
 /usr/bin/node node_modules/gulp/bin/gulp.js buildOnly
 /usr/bin/pm2 restart doc
+
