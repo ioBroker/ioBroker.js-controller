5288e65c77ce922250a94060be53e1b878ab09f2
AlCalzone <d.griesel@gmx.net>
2021-05-09 23:35:04 +0200

    fix: use resolveAdapterMainFile for adapter startup (#1337)
    * fix: use resolveAdapterMainFile for adapter startup
    
    * simplify logic as proposed

--- main.js
+++ main.js
@@ -3216,7 +3216,6 @@ async function startInstance(id, wakeUp) {
             });
     }
 
-    let fileName = instance.common.main || 'main.js';
     const adapterDir = tools.getAdapterDir(name);
     if (!fs.existsSync(adapterDir)) {
         procs[id].downloadRetry = procs[id].downloadRetry || 0;
@@ -3243,26 +3242,29 @@ async function startInstance(id, wakeUp) {
         args.push(`--max-old-space-size=${parseInt(instance.common.memoryLimitMB, 10)}`);
     }
 
-    let fileNameFull = path.join(adapterDir, fileName);
-
-    // workaround for old vis.
+    // workaround for old vis
     if (instance.common.onlyWWW && name === 'vis') {
         instance.common.onlyWWW = false;
     }
 
-    if (instance.common.mode !== 'extension' && (instance.common.onlyWWW || !fs.existsSync(fileNameFull))) {
-        fileName = name + '.js';
-        fileNameFull = path.join(adapterDir, fileName);
-        if (instance.common.onlyWWW || !fs.existsSync(fileNameFull)) {
-            // If not just www files
-            if (instance.common.onlyWWW || fs.existsSync(path.join(adapterDir, 'www'))) {
-                logger.debug(`${hostLogPrefix} startInstance ${name}.${args[0]} only WWW files. Nothing to start`);
-            } else {
-                logger.error(`${hostLogPrefix} startInstance ${name}.${args[0]}: cannot find start file!`);
-            }
+    // www-only adapters have no start file
+    if (instance.common.onlyWWW) {
+        logger.debug(`${hostLogPrefix} startInstance ${name}.${args[0]} only WWW files. Nothing to start`);
+        return;
+    }
+
+    /** @type {string | undefined} */
+    let adapterMainFile;
+    // Web extensions have a separate field for the main file. We don't need to search it in that case
+    if (instance.common.mode !== 'extension') {
+        try {
+            adapterMainFile = await tools.resolveAdapterMainFile(name);
+        } catch {
+            logger.error(`${hostLogPrefix} startInstance ${name}.${args[0]}: cannot find start file!`);
             return;
         }
     }
+
     procs[id].downloadRetry = 0;
 
     // read node.js engine requirements
@@ -3578,8 +3580,8 @@ async function startInstance(id, wakeUp) {
                     }
                     if (!procs[id].process) { // We were not able or should not start as compact mode
                         try {
-                            procs[id].process = cp.fork(fileNameFull, args, {
-                                execArgv: tools.getDefaultNodeArgs(fileNameFull),
+                            procs[id].process = cp.fork(adapterMainFile, args, {
+                                execArgv: tools.getDefaultNodeArgs(adapterMainFile),
                                 stdio: ['ignore', 'ignore', 'pipe', 'ipc'],
                                 windowsHide: true,
                                 cwd: adapterDir
@@ -3646,19 +3648,19 @@ async function startInstance(id, wakeUp) {
                         states.setState(id + '.sigKill', {val: 0, ack: false, from: hostObjectPrefix}, () => {
                             const _instance = (instance && instance._id && instance.common) ? instance._id.split('.').pop() || 0 : 0;
                             const logLevel = (instance && instance._id && instance.common) ? instance.common.loglevel || 'info' : 'info';
-                            if (fileNameFull) {
+                            if (adapterMainFile) {
                                 try {
                                     decache = decache || require('decache');
-                                    decache(fileNameFull);
+                                    decache(adapterMainFile);
 
                                     // Prior to requiring the main file make sure that the esbuild require hook was loaded
                                     // if this is a TypeScript adapter
-                                    if (fileNameFull.endsWith('.ts')) {
+                                    if (adapterMainFile.endsWith('.ts')) {
                                         require('esbuild-register');
                                     }
 
                                     procs[id].process = {
-                                        logic: require(fileNameFull)({
+                                        logic: require(adapterMainFile)({
                                             logLevel,
                                             compactInstance: _instance,
                                             compact: true
@@ -3871,7 +3873,7 @@ async function startInstance(id, wakeUp) {
             procs[id].schedule = schedule.scheduleJob(instance.common.schedule, () => {
                 // queue up, but only if not already queued
                 scheduledInstances[id] = {
-                    fileNameFull,
+                    fileNameFull: adapterMainFile,
                     adapterDir,
                     wakeUp
                 };
@@ -3882,8 +3884,8 @@ async function startInstance(id, wakeUp) {
             //noinspection JSUnresolvedVariable
             if (instance.common.allowInit) {
                 try {
-                    procs[id].process = cp.fork(fileNameFull, args, {
-                        execArgv: tools.getDefaultNodeArgs(fileNameFull),
+                    procs[id].process = cp.fork(adapterMainFile, args, {
+                        execArgv: tools.getDefaultNodeArgs(adapterMainFile),
                         windowsHide: true,
                         cwd: adapterDir
                     });
