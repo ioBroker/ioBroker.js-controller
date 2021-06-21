ef77a5afd93ffb36e691b072a3884bc9c592af3e
Max Hauser <moritz.heusinger@gmail.com>
2021-06-21 23:23:05 +0200

    Change tests to actually use a separate process to not bypass parsing of arguments (#1386)
    * partially change tests to cli calls
    
    * also change the db on cli level
    
    * cli now uses correct namespace
    
    * change some more tests to exec
    
    * first issue found due to real cli tests
    
    - fix yargs passing backup name as number
    
    * fix test setup for states db
    
    * finish new tests
    
    * increase repo timeout
    
    * increase user passwd test timeout

--- test/lib/setup4controller.js
+++ test/lib/setup4controller.js
@@ -34,6 +34,17 @@ function startController(options, callback) {
 
     console.log('startController...');
 
+    // adjust db for the cli tests
+    const iobrokerJSON = fs.readJSONSync(path.join(rootDir, 'data', 'iobroker.json'));
+    iobrokerJSON.objects.type = options.objects.type || 'file';
+    iobrokerJSON.objects.port = (options.objects.port === undefined) ? 19001 : options.objects.port;
+    iobrokerJSON.objects.host = options.objects.host || '127.0.0.1';
+    iobrokerJSON.objects.redisNamespace = options.objects.redisNamespace || '';
+    iobrokerJSON.states.type = options.states.type || 'file';
+    iobrokerJSON.states.port = (options.states.port === undefined) ? 19000 : options.states.port;
+    iobrokerJSON.states.host = options.states.host || '127.0.0.1';
+    fs.writeJSONSync(path.join(rootDir, 'data', 'iobroker.json'), iobrokerJSON, {spaces: 2});
+
     const settingsObjects = {
         connection: {
             type:               options.objects.type || 'file',
