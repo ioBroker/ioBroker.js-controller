2b8004315a2139abaec861c18dacbe7208b4bc40
Moritz Heusinger <moritz.heusinger@gmail.com>
2021-08-25 21:54:32 +0200

    fix sentinel test

--- packages/controller/test/redis-socket/setup-socket.sh
+++ packages/controller/test/redis-socket/setup-socket.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 redis-cli SHUTDOWN
-sudo redis-server packages/core/test/redis-socket/redis-socket.config &
+sudo redis-server packages/controller/test/redis-socket/redis-socket.config &
 sleep 15
 redis-cli -s /var/run/redis.sock PING
 npm run test-redis-socket
