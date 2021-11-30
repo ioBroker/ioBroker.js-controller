44089655b75677d0032912ed0161cd1fbdd1a8d8
Max Hauser <moritz.heusinger@gmail.com>
2021-11-30 17:08:17 +0000

    rm testing workaround for objects/states server subscribe issue (#1548)
    * rm testing workaround for objects/states server subscribe issue
    
    - closes #1457
    - reverts https://github.com/foxriver76/ioBroker.objects-redis/commit/2d358e2da7edfbf54e7fda267a0c62d1aa3a61d1
    - should be fixed by https://github.com/ioBroker/testing/commit/093b62cd2c89560a588f3f9d88a4296707f6927a and all adapters need 2.5.0 because of monopreo
    
    * additionally to reverting the commit we have no this stuff in new jsonl db too

--- packages/db-objects-jsonl/lib/objects/objectsInMemServerClass.js
+++ packages/db-objects-jsonl/lib/objects/objectsInMemServerClass.js
@@ -21,22 +21,7 @@ class ObjectsInMemoryServerClass extends ObjectsInRedisClient {
 
     constructor(settings) {
         settings.autoConnect = false; // delay Client connection to when we need it
-
-        // hack around testing problem where subscribe was called before connect
-        // Should be removed for a later release
-        const origConnected = settings.connected;
-        settings.connected = () => {
-            this.clientConnected = true;
-            if (Array.isArray(this.storedSubscribes) && this.storedSubscribes.length) {
-                this.log.warn(`${this.namespace} Replay ${this.storedSubscribes.length} subscription calls for Objects Server that were done before the client was connected initially`);
-                this.storedSubscribes.forEach((s => this.subscribe(s.pattern, s.options, s.callback)));
-                this.storedSubscribes = [];
-            }
-            origConnected();
-        };
         super(settings);
-        this.clientConnected = false;
-        this.storedSubscribes = [];
 
         const serverSettings = {
             namespace:  settings.namespace ? `${settings.namespace}-Server` : 'Server',
@@ -65,14 +50,6 @@ class ObjectsInMemoryServerClass extends ObjectsInRedisClient {
 
     dirExists(id, name) {
         return this.objectsServer.dirExists(id, name);
-    }
-
-    subscribe(pattern, options, callback) {
-        if (!this.clientConnected) {
-            this.storedSubscribes.push({pattern, options, callback}); // we ignore the promise return because not used for this testing issue we work around here
-        } else {
-            return super.subscribe(pattern, options, callback);
-        }
     }
 }
 module.exports = ObjectsInMemoryServerClass;
