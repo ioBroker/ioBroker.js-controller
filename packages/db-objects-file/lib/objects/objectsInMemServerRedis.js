71301ee6d6d0c153c92c97a3e900922d58e9da28
Moritz Heusinger <moritz.heusinger@gmail.com>
2021-12-04 17:57:34 +0100

    okay.. ;-)

--- packages/db-objects-file/lib/objects/objectsInMemServerRedis.js
+++ packages/db-objects-file/lib/objects/objectsInMemServerRedis.js
@@ -796,10 +796,9 @@ class ObjectsInMemoryServer extends ObjectsInMemoryFileDB {
             } else { // such a request should never happen
                 handler.sendArray(responseId, isScan ? ['0', []] : []); // send out file or full db response
             }
-        } else if (namespace === this.setNamespace) {
+        } else if (namespace === this.namespaceSet) {
             handler.sendArray(responseId, isScan ? ['0', []] : []); // send out empty array, we have no sets
         } else {
-            this.log.error('tesssst ' + this.namespaceSet)
             handler.sendError(responseId, new Error(`${isScan ? 'SCAN' : 'KEYS'}-UNSUPPORTED for namespace ${namespace}: Pattern=${pattern}`));
         }
     }
