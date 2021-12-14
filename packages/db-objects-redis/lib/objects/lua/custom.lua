4f7cb505dcb93a10d0e584fff906db650dfd2ed5
Max Hauser <moritz.heusinger@gmail.com>
2021-12-14 12:13:57 +0100

    add set for custom object view (#1593)
    * add set for custom object view
    
    - closes #1572
    
    * comments
    
    * add set handling to setObj
    
    * fix typo

--- packages/db-objects-redis/lib/objects/lua/custom.lua
+++ packages/db-objects-redis/lib/objects/lua/custom.lua
@@ -8,20 +8,13 @@ cursor = result[1]
 local keys = result[2]
 local argStart = KEYS[1] .. KEYS[2]
 local argEnd = KEYS[1] .. KEYS[3]
-local checkStr = string.format("%q:{", "custom")
 --  function(doc) {
 --      if (doc.type==="state" && (doc.common.custom || doc.common.history))
 --          emit(doc._id, doc.common.custom || doc.common.history)
 --   }
 for _, key in ipairs(keys) do
     if (key >= argStart and key < argEnd) then
-        local obj = redis.call("get", key)
-        if (obj:find(checkStr) ~= nil) then
-            local success, decoded = pcall(cjson.decode, obj)
-            if (success and decoded.common ~= nil and decoded.common.custom ~= nil) then
-                rep[#rep + 1] = obj
-            end
-        end
+        rep[#rep + 1] = redis.call("get", key)
     end
 end
 return { rep, cursor }
