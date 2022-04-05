6618176d49bc5077a5b08e3542e366a0f1b11f9d
Moritz Heusinger <moritz.heusinger@gmail.com>
2022-04-05 21:05:27 +0200

    rm not implemented and flagged for removage methods

--- packages/adapter/src/lib/adapter/adapter.js
+++ packages/adapter/src/lib/adapter/adapter.js
@@ -8775,25 +8775,6 @@ class AdapterClass extends EventEmitter {
             });
         };
 
-        // read all logs prepared for this adapter at start
-        const readLogs = callback => {
-            if (!adapterStates) {
-                // if states is no longer existing, we do not need to unsubscribe
-                this.log.info('readLogs not processed because States database not connected');
-                return tools.maybeCallbackWithError(callback, tools.ERRORS.ERROR_DB_CLOSED);
-            }
-
-            // read all stored messages
-            adapterStates.getLog('system.adapter.' + this.namespace, (err, msg) => {
-                if (msg) {
-                    this.emit('log', msg);
-                    setImmediate(() => readLogs(callback));
-                } else {
-                    return tools.maybeCallback(callback);
-                }
-            });
-        };
-
         // debug function to find error with stop logging
         const checkLogging = () => {
             let logs = [];
@@ -8998,8 +8979,6 @@ class AdapterClass extends EventEmitter {
                 this.processLog = msg => {
                     msg && !this._stopInProgress && this.emit('log', msg);
                 };
-
-                readLogs();
 
                 adapterStates.subscribeLog('system.adapter.' + this.namespace);
             } else {
