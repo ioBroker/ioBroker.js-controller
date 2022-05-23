27609d9c897dc5d79a8c0ba802a0c6157ae25bdc
GermanBluefox <dogafox@gmail.com>
2022-05-23 18:42:33 +0200

    Start service if available by iob.bat start/stop

--- packages/controller/iobroker.bat
+++ packages/controller/iobroker.bat
@@ -1,2 +1,18 @@
 @echo off
-if %1==fix (npx @iobroker/fix) else (node iobroker.js %1 %2 %3 %4 %5 %6 %7 %8)
+if %1==fix (
+    npx @iobroker/fix
+) else (
+    if exist serviceIoBroker.bat (
+        if %1==start (
+            call serviceIoBroker.bat start
+        ) else (
+            if %1==stop (
+                call serviceIoBroker.bat stop
+            ) else (
+                node iobroker.js %1 %2 %3 %4 %5 %6 %7 %8
+            )
+        )
+    ) else (
+        node iobroker.js %1 %2 %3 %4 %5 %6 %7 %8
+    )
+)
