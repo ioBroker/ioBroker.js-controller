ioBroker/ioBroker.js-controller

commit 0732666ca1b6d32dffd417cf6405b142e684890b

Formatting

packages/common/src/lib/common/logger.ts
@@ -221,7 +221,7 @@ export function logger(
                             ? transport.symlinkName
                             : path.basename(`${transport.filename}.current.log`);
 
-                    transport.filename += '.%DATE%' + (transport.fileext || '');
+                    transport.filename += `.%DATE%${transport.fileext || ''}`;
                     //transport.label       = prefix || ''; //TODO format.label()
                     //                    transport.json        = (transport.json      !== undefined) ? transport.json      : false; // TODO format.json(), new Default!!
                     transport.silent = transport.silent !== undefined ? transport.silent : false;
@@ -244,7 +244,7 @@ export function logger(
                         : true;
 
                     if (transport.maxFiles === null && userOptions.maxDays) {
-                        transport.maxFiles = userOptions.maxDays + 'd';
+                        transport.maxFiles = `${userOptions.maxDays}d`;
                     }
 
                     try {
@@ -355,7 +355,7 @@ export function logger(
     } else {
         for (let i = 0; i < files.length; i++) {
             const opt = {
-                name: i ? 'dailyRotateFile' + i : tools.appName,
+                name: i ? `dailyRotateFile${i}` : tools.appName,
                 filename: path.normalize(
                     isNpm ? `${__dirname}/../../../log/${files[i]}` : `${__dirname}/../log/${files[i]}`
                 ),
@@ -407,7 +407,7 @@ export function logger(
             /** @ts-expect-error we use undocumented stuff here TODO */
             transport = transport.transport ? transport.transport : transport;
             /** @ts-expect-error we use undocumented stuff here TODO */
-            return transport.dirname + '/' + transport.filename.replace('%DATE%', getDate());
+            return `${transport.dirname}/${transport.filename.replace('%DATE%', getDate())}`;
         } else {
             return '';
         }
@@ -472,20 +472,20 @@ export function logger(
                                         });
                                         console.log(`host.${hostname} Delete log file ${files[i]}`);
                                         /** @ts-expect-error we use undocumented stuff here TODO */
-                                        fs.unlinkSync(transport.dirname + '/' + files[i]);
+                                        fs.unlinkSync(`${transport.dirname}/${files[i]}`);
                                     } catch (e) {
                                         // there is a bug under windows, that file stays opened and cannot be deleted
                                         this.log({
                                             level: os.platform().startsWith('win') ? 'info' : 'error',
                                             message: `host.${hostname} Cannot delete file "${path.normalize(
                                                 /** @ts-expect-error we use undocumented stuff here TODO */
-                                                transport.dirname + '/' + files[i]
+                                                `${transport.dirname}/${files[i]}`
                                             )}": ${e}`
                                         });
                                         console.log(
                                             `host.${hostname} Cannot delete file "${path.normalize(
                                                 /** @ts-expect-error we use undocumented stuff here TODO */
-                                                transport.dirname + '/' + files[i]
+                                                `${transport.dirname}/${files[i]}`
                                             )}": ${e.message}`
                                         );
                                     }
@@ -506,63 +506,19 @@ export function logger(
 
 function getDate(): string {
     const ts = new Date();
-    let result = ts.getFullYear() + '-';
-    let value: number | string = ts.getMonth() + 1;
-    if (value < 10) {
-        value = '0' + value;
-    }
-    result += value + '-';
-
-    value = ts.getDate();
-    if (value < 10) {
-        value = '0' + value;
-    }
-    result += value;
-    return result;
+    return `${ts.getFullYear()}-${(ts.getMonth() + 1).toString().padStart(2, '0')}-${ts.getDate().toString().padStart(2, '0')}`;
 }
 
 function timestamp(date: string): string {
     const ts = date ? new Date(date) : new Date();
-    let result = ts.getFullYear() + '-';
-
-    let value: string | number = ts.getMonth() + 1;
-    if (value < 10) {
-        value = '0' + value;
-    }
-    result += value + '-';
-
-    value = ts.getDate();
-    if (value < 10) {
-        value = '0' + value;
-    }
-    result += value + ' ';
-
-    value = ts.getHours();
-    if (value < 10) {
-        value = '0' + value;
-    }
-    result += value + ':';
-
-    value = ts.getMinutes();
-    if (value < 10) {
-        value = '0' + value;
-    }
-    result += value + ':';
-
-    value = ts.getSeconds();
-    if (value < 10) {
-        value = '0' + value;
-    }
-    result += value + '.';
-
-    value = ts.getMilliseconds();
-    if (value < 10) {
-        value = '00' + value;
-    } else if (value < 100) {
-        value = '0' + value;
-    }
-
-    result += value + ' ';
-
-    return result;
+    return `${ts.getFullYear()}-${(ts.getMonth() + 1).toString().padStart(2, '0')}-${ts
+        .getDate()
+        .toString()
+        .padStart(2, '0')} ${ts.getHours().toString().padStart(2, '0')}:${ts
+        .getMinutes()
+        .toString()
+        .padStart(2, '0')}:${ts.getSeconds().toString().padStart(2, '0')}.${ts
+        .getMilliseconds()
+        .toString()
+        .padStart(3, '0')} `;
 }

packages/common/src/lib/common/maybeCallback.ts
@@ -122,7 +122,7 @@ export function maybeCallbackWithError<
  *
  * @param callback - callback function to be executed
  * @param error - error which will be used by the callback function. If callback is not a function and
- * error is given, a rejected Promise is returned. If error is given but it is not an instance of Error, it is converted into one.
+ * error is given, a rejected Promise is returned. If error is given, but it is not an instance of Error, it is converted into one.
  * @param args - as many arguments as needed, which will be returned by the callback function or by the Promise
  * @returns if Promise is resolved with multiple arguments, an array is returned
  */

packages/common/src/lib/common/tools.ts
@@ -2099,9 +2099,9 @@ export function getControllerDir(): string {
     }
 
     // Apparently, checking vs null/undefined may miss the odd case of controllerPath being ""
-    // Thus we check for falsyness, which includes failing on an empty path
+    // Thus we check for falseness, which includes failing on an empty path
     let checkPath = path.join(__dirname, '../..');
-    // Also check in the current check dir (along with iobroker.js-controller subdirs)
+    // Also check in the current check dir (along with iobroker.js-controller sub-dirs)
     possibilities.unshift('');
     while (true) {
         for (const pkg of possibilities) {
@@ -2111,7 +2111,7 @@ export function getControllerDir(): string {
                     return possiblePath;
                 }
             } catch {
-                // not found, continue with next possiblity
+                // not found, continue with next possibility
             }
         }
 
@@ -2774,14 +2774,14 @@ export function parseDependencies(
                 // No version given, all are okay
                 adapters[rule] = '*';
             } else if (isObject(rule)) {
-                // can be object containing single adapter or multiple
+                // can be if object containing single adapter or multiple
                 Object.keys(rule)
                     .filter(adapter => !adapters[adapter])
                     .forEach(adapter => (adapters[adapter] = rule[adapter]));
             }
         });
     } else if (typeof dependencies === 'string') {
-        // its a single string without version requirement
+        // it's a single string without version requirement
         adapters[dependencies] = '*';
     } else if (isObject(dependencies)) {
         // if dependencies is already an object, just use it
@@ -2791,12 +2791,12 @@ export function parseDependencies(
 }
 
 /**
- * Validates types of obj.common properties and object.type, if invalid types are used, an error is thrown.
- * If attributes of obj.common are not provided, no error is thrown. obj.type has to be there and has to be valid.
+ * Validates types of `obj.common` properties and object.type, if invalid types are used, an error is thrown.
+ * If attributes of `obj.common` are not provided, no error is thrown. obj.type has to be there and has to be valid.
  *
  * @param obj an object which will be validated
  * @param extend (optional) if true checks will allow more optional cases for extendObject calls
- * @throws Error if a property has the wrong type or obj.type is non-existing
+ * @throws Error if a property has the wrong type or `obj.type` is non-existing
  */
 export function validateGeneralObjectProperties(obj: any, extend?: boolean): void {
     // designs have no type but have attribute views
@@ -3218,21 +3218,21 @@ export function parseShortGithubUrl(url: string): ParsedGithubUrl | null {
     };
 }
 
-/** This is used to parse the pathname of a github URL */
+/** This is used to parse the pathname of a GitHub URL */
 const githubPathnameRegex =
     /^\/(?<user>[^/]+)\/(?<repo>[^/]*?)(?:\.git)?(?:\/(?:tree|tarball|archive)\/(?<commit>.*?)(?:\.(?:zip|gz|tar\.gz))?)?$/;
 
 /**
  * Tests if the given pathname matches the format /<githubname>/<githubrepo>[.git][/<tarball|tree|archive>/<commit-ish>[.zip|.gz]]
- * @param pathname The pathname part of a Github URL
+ * @param pathname The pathname part of a GitHub URL
  */
 export function isGithubPathname(pathname: string): boolean {
     return githubPathnameRegex.test(pathname);
 }
 
 /**
- * Tries to a github pathname format /<githubname>/<githubrepo>[.git][/<tarball|tree|archive>/<commit-ish>[.zip|.gz|.tar.gz]] into its separate parts
- * @param pathname The pathname part of a Github URL
+ * Tries to a GitHub pathname format /<githubname>/<githubrepo>[.git][/<tarball|tree|archive>/<commit-ish>[.zip|.gz|.tar.gz]] into its separate parts
+ * @param pathname The pathname part of a GitHub URL
  */
 export function parseGithubPathname(pathname: string): ParsedGithubUrl | null {
     const match = githubPathnameRegex.exec(pathname);
@@ -3262,7 +3262,7 @@ export function removePreservedProperties(
             // we have to go one step deeper
             removePreservedProperties(preserve[prop], oldObj[prop], newObj[prop]);
         } else if (newObj && newObj[prop] !== undefined && oldObj && oldObj[prop] !== undefined) {
-            // we only need to remove something if its in the old object and in the new one
+            // we only need to remove something if it's in the old object and in the new one
             if (typeof preserve[prop] === 'boolean') {
                 delete newObj[prop];
             } else if (Array.isArray(preserve[prop])) {

packages/controller/src/lib/session.js
@@ -22,11 +22,10 @@ module.exports = function (session, defaultTtl) {
     /**
      * Attempt to fetch session by the given `sid`.
      *
-     * @param {String} sid
-     * @param {Function} fn
+     * @param {String} sid Session ID
+     * @param {Function} fn callback
      * @api public
      */
-
     AdapterStore.prototype.get = function (sid, fn) {
         this.adapter.getSession(sid, obj => {
             if (obj) {
@@ -44,9 +43,10 @@ module.exports = function (session, defaultTtl) {
     /**
      * Commit the given `sess` object associated with the given `sid`.
      *
-     * @param {String} sid
+     * @param {String} sid Session ID
+     * @param {Number} ttl Time to live
      * @param {Session} sess
-     * @param {Function} fn
+     * @param {Function} fn callback
      * @api public
      */
     AdapterStore.prototype.set = function (sid, ttl, sess, fn) {
@@ -68,10 +68,10 @@ module.exports = function (session, defaultTtl) {
     /**
      * Destroy the session associated with the given `sid`.
      *
-     * @param {String} sid
+     * @param {String} sid Session ID
+     * @param {Function} fn callback
      * @api public
      */
-
     AdapterStore.prototype.destroy = function (sid, fn) {
         this.adapter.destroySession(sid, fn);
     };

commit 7437f8bc14b83d50a7692936908a15bf530a025d

fixed lint issues introduced in https://github.com/ioBroker/ioBroker.js-controller/commit/0732666ca1b6d32dffd417cf6405b142e684890b

packages/common/src/lib/common/logger.ts
@@ -506,7 +506,10 @@ export function logger(
 
 function getDate(): string {
     const ts = new Date();
-    return `${ts.getFullYear()}-${(ts.getMonth() + 1).toString().padStart(2, '0')}-${ts.getDate().toString().padStart(2, '0')}`;
+    return `${ts.getFullYear()}-${(ts.getMonth() + 1).toString().padStart(2, '0')}-${ts
+        .getDate()
+        .toString()
+        .padStart(2, '0')}`;
 }
 
 function timestamp(date: string): string {