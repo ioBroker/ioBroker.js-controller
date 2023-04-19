ef0d2b07f87cb18b8e616e9949dc05e6628ca0ee
foxriver76 <moritz.heusinger@gmail.com>
2023-04-19 09:42:45 +0200

    refactor the diag info vis part and port to ts

--- packages/cli/src/lib/setup/setupBackup.ts
+++ packages/cli/src/lib/setup/setupBackup.ts
@@ -104,7 +104,6 @@ export class BackupRestore {
         fs.ensureDirSync(destPath);
 
         try {
-            // @ts-expect-error #1917
             const res = await this.objects.readDirAsync(id, srcPath);
             if (res) {
                 for (const entry of res) {
