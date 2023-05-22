e443d74cfb64e626299f2c6d44ac1e5366344082
foxriver76 <moritz.heusinger@gmail.com>
2023-05-22 09:06:29 +0200

    also add getHistory to supportedMessages
    - and also detect messagebox support if number is used on stopInstance

--- packages/adapter/src/lib/adapter/utils.ts
+++ packages/adapter/src/lib/adapter/utils.ts
@@ -2,6 +2,7 @@ import { isObject } from '@iobroker/js-controller-common/tools';
 
 /**
  * Check if messagebox is configured for given instance
+ * This means, at least one of the properties has a value different from false
  *
  * @param instanceCommon Instance common
  */
@@ -10,5 +11,5 @@ export function isMessageboxSupported(instanceCommon: ioBroker.InstanceCommon): 
         return !!instanceCommon.messagebox;
     }
 
-    return Object.values(instanceCommon.supportedMessages).includes(true);
+    return Object.values(instanceCommon.supportedMessages).find(val => val !== false) !== undefined;
 }
