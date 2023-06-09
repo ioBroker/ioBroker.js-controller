f6490dc2d5c10ecb60c973092d7cc2ff79e4ab0f
Marc Lammers <marc@lammers.dev>
2023-06-09 21:12:59 +0200

    Changes after review.

--- packages/adapter/src/lib/adapter/adapter.ts
+++ packages/adapter/src/lib/adapter/adapter.ts
@@ -2367,25 +2367,19 @@ export class AdapterClass extends EventEmitter {
         return this._updateConfig({ newConfig });
     }
 
-    private async _updateConfig(options: InternalUpdateConfigOptions, useAutoEncryption?: boolean): ioBroker.SetObjectPromise {
+    private async _updateConfig(options: InternalUpdateConfigOptions): ioBroker.SetObjectPromise {
         // merge the old and new configuration
         let _config = Object.assign({}, this.config, options.newConfig);
 
-        if(useAutoEncryption) {
-            // Encrypt all attributes of encryptedNative before update the config
-            if (Array.isArray(this.adapterConfig.encryptedNative)) {
-                for (const attr of this.adapterConfig.encryptedNative) {
-                    // we can only decrypt strings
-                    if (typeof this.config[attr] === 'string') {
-                        _config[attr] = this.encrypt(this.config[attr])
-                    }
+        // Encrypt all attributes of encryptedNative before update the config
+        if (Array.isArray(this.adapterConfig.encryptedNative)) {
+            for (const attr of this.adapterConfig.encryptedNative) {
+                // we can only decrypt strings
+                if (typeof _config[attr] === 'string') {
+                    _config[attr] = this.encrypt(_config[attr])
                 }
             }
-        } else {
-            this._logger.warn('Auto encryption is not used. Values from encryptedNative are overwritten with decrypted value.'+
-            'Please use useAutoEncryption parameter.');
         }
-        
 
         // update the adapter config object
         const configObjId = `system.adapter.${this.namespace}`;
