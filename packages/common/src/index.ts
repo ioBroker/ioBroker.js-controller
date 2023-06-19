b416a9bc5ee4abc941677924f63a1ca83e8291d2
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-06-19 10:05:43 +0200

    fix: build error

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,5 @@
+/// <reference types="@iobroker/types-dev" />
+
 export * as tools from './lib/common/tools';
 export { EXIT_CODES } from './lib/common/exitCodes';
 export { password } from './lib/common/password';
