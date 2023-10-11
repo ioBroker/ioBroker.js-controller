92dcc26d2fe88a2768e7d565a21e090d8e850557
GermanBluefox <dogafox@gmail.com>
2023-10-11 09:10:44 +0200

    Just corrected some comments

--- packages/adapter/src/lib/adapter/constants.ts
+++ packages/adapter/src/lib/adapter/constants.ts
@@ -22,7 +22,7 @@ export enum STATE_QUALITY {
     BAD = 0x01,
     /** The instance cannot establish a connection */
     CONNECTION_PROBLEM = 0x02,
-    /** Substitute value from controller, do not set this in adapters */
+    /** Substitute value from controller. Do not set this in adapters */
     SUBSTITUTE_FROM_CONTROLLER = 0x10,
     /** Quality for default values */
     SUBSTITUTE_INITIAL_VALUE = 0x20,
@@ -55,7 +55,7 @@ const SUPPORTED_FEATURES_INTERNAL = [
     'ADAPTER_GETPORT_BIND', // getPort method of adapter supports second parameter to bind to a special network interface, Since js-controller 2.0
     'ADAPTER_DEL_OBJECT_RECURSIVE', // delObject supports options.recursive flag to delete objects structures recursive, Since js-controller 2.2
     'ADAPTER_SET_OBJECT_SETS_DEFAULT_VALUE', // setObject(*) methods set the default (def) value via setState after the object is created. Since js-controller 2.0
-    'ADAPTER_AUTO_DECRYPT_NATIVE', // all native attributes, that are listed in an array `encryptedNative` in io-pack will be automatically decrypted and encrypted. Since js-controller 3.0
+    'ADAPTER_AUTO_DECRYPT_NATIVE', // all native attributes that are listed in an array `encryptedNative` in io-pack will be automatically decrypted and encrypted. Since js-controller 3.0
     'PLUGINS', // configurable plugins supported. Since `js-controller` 3.0
     'CONTROLLER_NPM_AUTO_REBUILD', // Automatic rebuild when node version mismatch is detected. Since `js-controller` 3.0
     'CONTROLLER_READWRITE_BASE_SETTINGS', // If base settings could be read and written. Since js-controller 3.0
@@ -64,9 +64,9 @@ const SUPPORTED_FEATURES_INTERNAL = [
     'DEL_INSTANCE_CUSTOM', // instances/adapter can be deleted with --custom flag to remove corresponding custom of all objects. Since js-controller 4.0
     'BINARY_STATE_EVENT', // stateChange event could be received for binary states too. Deprecated in js-controller 5.0
     'CUSTOM_FULL_VIEW', // `getObjectView('system', 'custom-full', ...)` will return full objects and not only `common.custom` part. Since `js-controller` 5.0
-    'ADAPTER_GET_OBJECTS_BY_ARRAY', // getForeignObjects supports array of ids too. Since js-controller 5.0
+    'ADAPTER_GET_OBJECTS_BY_ARRAY', // getForeignObjects supports an array of ids too. Since js-controller 5.0
     'CONTROLLER_UI_UPGRADE', // Controller can be updated via sendToHost('upgradeController', ...)
-    'ADAPTER_WEBSERVER_UPGRADE' // Controller supports upgrading adapter and providing a webserver (triggered via sendToHost). Since `js-controller` 5.0
+    'ADAPTER_WEBSERVER_UPGRADE' // Controller supports upgrading adapter and provides a webserver (triggered via sendToHost). Since `js-controller` 5.0
 ] as const;
 
 export const SUPPORTED_FEATURES = [...SUPPORTED_FEATURES_INTERNAL];
