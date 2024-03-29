70cd2705e39066a4d02b65390aa9874c7b8fcb1a
GermanBluefox <dogafox@gmail.com>
2024-03-29 11:24:27 +0800

    Groups and users can have name as StringOrTranslated too (e.g. admins)

--- packages/types-dev/objects.d.ts
+++ packages/types-dev/objects.d.ts
@@ -419,7 +419,7 @@ declare global {
 
         interface UserCommon extends ObjectCommon {
             /** The username */
-            name: string;
+            name: StringOrTranslated;
             /** The hashed password */
             password: string;
             /** Whether this user is enabled */
@@ -431,7 +431,7 @@ declare global {
 
         interface GroupCommon extends ObjectCommon {
             /** The name of this group */
-            name: string;
+            name: StringOrTranslated;
             /** The users of this group */
             members: ObjectIDs.User[]; // system.user.name, ...
             /** The default permissions of this group */
