263324edcbb6e5713bfbefb57eddeeb9c47a2bbf
foxriver76 <moritz.heusinger@gmail.com>
2024-05-03 21:32:36 +0200

    refactor variable name

--- packages/cli/src/lib/setup/setupPacketManager.ts
+++ packages/cli/src/lib/setup/setupPacketManager.ts
@@ -235,7 +235,7 @@ export class PacketManager {
             return [];
         }
 
-        const packagesList = res.split('\n').filter(packageName => packageName !== '');
+        const packagesList = res.split('\n').filter(packageInfo => packageInfo !== '');
         // first line is no package, just Listing...
         packagesList.shift();
 
