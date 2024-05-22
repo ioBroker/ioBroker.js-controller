947e25214e2be528849d4618c05c4f5edd5580da
foxriver76 <moritz.heusinger@gmail.com>
2024-05-22 15:26:56 +0200

    exported session so adapters can use it like before after ESM port

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -5,6 +5,7 @@ export { password } from '@/lib/common/password.js';
 export { logger } from '@/lib/common/logger.js';
 export { defaultRedisInterview } from '@/lib/common/interview.js';
 export * as constants from '@/lib/common/constants.js';
+export { createAdapterStore as session } from '@/lib/common/session.js';
 
 /** This is a backward compatibility shim, if all adapters are on adapter-core 2.6.11 or 3.1.4 remove this */
 export const letsencrypt = null;
