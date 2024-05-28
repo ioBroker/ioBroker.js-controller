148ca04e044b08522573daab3f55dd410bc0e01b
foxriver76 <moritz.heusinger@gmail.com>
2024-05-28 21:51:39 +0200

    fix import

--- packages/db-objects-redis/src/lib/objects/objectsInRedisClient.ts
+++ packages/db-objects-redis/src/lib/objects/objectsInRedisClient.ts
@@ -24,7 +24,7 @@ import type {
 import * as utils from '@/lib/objects/objectsUtils.js';
 import semver from 'semver';
 import * as CONSTS from '@/lib/objects/constants.js';
-import type { InternalLogger } from '@iobroker/js-controller-common/tools';
+import type { InternalLogger } from '@iobroker/js-controller-common-db/tools';
 import type { ConnectionOptions, DbStatus } from '@iobroker/db-base/inMemFileDB';
 
 import * as url from 'node:url';
