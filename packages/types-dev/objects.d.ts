d2129c4edbf5601e1ce5cc0871fa2385afc1622c
Bluefox <dogafox@gmail.com>
2024-06-22 18:38:27 +0900

    Update objects.d.ts

--- packages/types-dev/objects.d.ts
+++ packages/types-dev/objects.d.ts
@@ -743,9 +743,9 @@ declare global {
             /** If floating comma is used instead of dot */
             isFloatComma: boolean;
             /** Configured longitude */
-            longitude: string;
+            longitude: number;
             /** Configured latitude */
-            latitude: string;
+            latitude: number;
             /** Optional user's city (only for diagnostics) */
             city?: string;
             /** Optional user's country (only for diagnostics) */
